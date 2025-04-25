9ea1b6ec985e6fb2b9b40491a7fe83da7b81804b
madhan45-mad <madhanmad476@gmail.com>
2025-04-25 17:26:43 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,11 @@
+package com.example.mutualfollowers.model;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private List<Integer> follows;
+
+    // Getters and setters
+}
